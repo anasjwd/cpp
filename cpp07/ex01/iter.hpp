f91fcdd365dcabbeec465b769634c76f03b8f342
ajawad <ajawad@e2r2p10.1337.ma>
2025-03-22 03:09:29 +0000

    Implement a new iter that accepts a const

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -6,3 +6,10 @@ void iter(T* array, unsigned int length, void (*func)(T&))
 	for (unsigned int i = 0; i < length; i++)
 		func(array[i]);
 }
+
+template <typename T>
+void iter(const T* array, unsigned int length, void (*func)(const T&))
+{
+	for (unsigned int i = 0; i < length; i++)
+		func(array[i]);
+}
